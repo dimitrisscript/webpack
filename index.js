269c7afc22d30e262759d0ce3513b110d56efb75
Dimitris Siakavelis <dimitris.siakavelis@dpdk.com>
2020-05-24 14:09:52 +0200

    initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+console.log('afa');
+
+// const age = 100;
+const people = ['web', 'kait'];
+
+people.map(name => {
+    console.log(`${name} Bos`);
+});
+
+const foo = Array.from([1, 23, 4, 5]);
+
+
+
+console.log('foo', foo);
